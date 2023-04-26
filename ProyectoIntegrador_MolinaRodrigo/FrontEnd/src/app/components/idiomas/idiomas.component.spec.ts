b167589385b83434ff6c93a4b5a2970731cec90b
elrodrics <elrodri1525@gmail.com>
2023-04-26 17:00:54 -0300

    creacion de la carpeta front end

--- ProyectoIntegrador_MolinaRodrigo/FrontEnd/src/app/components/idiomas/idiomas.component.spec.ts
+++ ProyectoIntegrador_MolinaRodrigo/FrontEnd/src/app/components/idiomas/idiomas.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SocialComponent } from './social.component';
+import { IdiomasComponent } from './idiomas.component';
 
-describe('SocialComponent', () => {
-  let component: SocialComponent;
-  let fixture: ComponentFixture<SocialComponent>;
+describe('IdiomasComponent', () => {
+  let component: IdiomasComponent;
+  let fixture: ComponentFixture<IdiomasComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SocialComponent ]
+      declarations: [ IdiomasComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SocialComponent);
+    fixture = TestBed.createComponent(IdiomasComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
