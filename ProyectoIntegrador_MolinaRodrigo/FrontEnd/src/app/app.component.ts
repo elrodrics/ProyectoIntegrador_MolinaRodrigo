f907fc074b9370cce41fb7aa7ed6468e75ba25b4
elrodrics <elrodri1525@gmail.com>
2023-04-27 21:09:54 -0300

    animaciones

--- ProyectoIntegrador_MolinaRodrigo/FrontEnd/src/app/app.component.ts
+++ ProyectoIntegrador_MolinaRodrigo/FrontEnd/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProyectoIntegrador_MolinaRodrigo';
+
+  ngOnInit(){
+    AOS.init();
+      window.addEventListener('load',AOS.refresh)
+    
+  }
+
 }
